685adb5d6322f4182d2269ff921a80365a61eb3c
Manoj Mutireddygari <mutireddymanoj2305@gmail.com>
2025-08-02 19:12:13 +0530

    firebase-config.js

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDdpYiE2b5A6_Pm2k8UCWNLhznH3knQzAc",
-    authDomain: "docguard-9913c.firebaseapp.com",
-    projectId: "docguard-9913c",
-    storageBucket: "docguard-9913c.firebasestorage.app",
-    messagingSenderId: "8989263658",
-    appId: "1:8989263658:web:f0ba6f56c91947224e4053",
-    measurementId: "G-4XNMKG26QR"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
